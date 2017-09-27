f5284fbbd066b172e118343d97b87767ec764ca5
Hoang Hiep <hoanghiepbk@gmail.com>
2017-09-27 09:55:00 +0700

    commit develop.php from develop branch

---
+++ t3.php
@@ -0,0 +1,20 @@
+<?php
+	$a = array(
+	'hiep' => array( 
+		'name' => 'A',
+		'position' => 'b',
+		),
+	'hoang' => array( 
+		'name' => 'c',
+		'position' => 'd',
+	
+	)
+	);
+	
+		
+		echo 'name:'.$a['hiep']['name'].'<br/>';
+		echo 'position:'.$a['hiep']['position'].'<br/>';
+		echo 'name:'.$a['hoang']['name'].'<br/>';
+		echo 'position:'.$a['hoang']['position'].'<br/>';
+	
+?>
