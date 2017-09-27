93e471e6a8d65dd1938bef15490175a3982352a9
Hoang Hiep <hoanghiepbk@gmail.com>
2017-09-27 11:38:16 +0700

    dangnahp

---
+++ dangnhap.php
@@ -0,0 +1,5 @@
+<form action="dangnhap.php" method="POST">
+Username: <input type="text" name="user"/><br />
+Password: <input type="password" name="pass"/><br />	
+<input type="submit" value="Dang Nhap" name="submit"/>
+</form>
